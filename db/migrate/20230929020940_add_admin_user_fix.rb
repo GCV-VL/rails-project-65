89c813cd61813d896fdaa4a2067b42ac42f36f8b
Kim Vladimir <kimvova85@mail.ru>
2023-09-29 12:10:05 +1000

    Fix admin user

--- db/migrate/20230929020940_add_admin_user_fix.rb
+++ db/migrate/20230929020940_add_admin_user_fix.rb
@@ -1,4 +1,4 @@
-class AddAdminUser < ActiveRecord::Migration[7.0]
+class AddAdminUserFix < ActiveRecord::Migration[7.0]
   def change
     user = User.find_or_initialize_by(email: 'kimvova85@mail.ru')
     user.update(name: 'Developer', admin: true)

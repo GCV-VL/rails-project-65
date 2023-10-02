8f914ee4ca147da3627fdcfb90e7d24e23f00768
Ким В. Д. 4025 <26870@7733.ru>
2023-10-02 20:53:43 +1000

    lint fixed

--- db/migrate/20230307070354_add_admin_to_user.rb
+++ db/migrate/20230307070354_add_admin_to_user.rb
@@ -2,6 +2,6 @@
 
 class AddAdminToUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :admin, :boolean, default: false
+    add_column :users, :admin, :boolean, null: false, default: false
   end
 end
--- db/migrate/20230929020940_add_admin_user_fix.rb
+++ db/migrate/20230929020940_add_admin_user_fix.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddAdminUserFix < ActiveRecord::Migration[7.0]
   def change
     user = User.find_or_initialize_by(email: 'kimvova85@mail.ru')

98f89b293f43586faecd6115c4ebd4f2fa0c6e3f
Ким В. Д. 4025 <26870@7733.ru>
2023-10-02 21:34:29 +1000

    migration changed

--- db/migrate/20230319131830_add_another_admin.rb
+++ db/migrate/20230319131830_add_another_admin.rb
@@ -2,7 +2,7 @@
 
 class AddAnotherAdmin < ActiveRecord::Migration[7.0]
   def change
-    user = User.find_or_initialize_by(email: 'al3xander.koval@gmail.com')
-    user.update(name: 'Проверяющий', admin: true)
+    user = User.find_or_initialize_by(email: 'vasiliqa13@gmail.com')
+    user.update(name: 'Vasilisa', admin: true)
   end
 end
--- db/migrate/20230929015830_add_admin_user.rb
+++ db/migrate/20230929015830_add_admin_user.rb
@@ -3,6 +3,6 @@
 class AddAdminUser < ActiveRecord::Migration[7.0]
   def change
     user = User.find_or_initialize_by(email: 'kimvova85@mail.ru')
-    user.update(name: 'Developer', admin: true)
+    user.update(name: 'Developer with another acc', admin: true)
   end
 end

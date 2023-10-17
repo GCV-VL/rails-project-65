aea99d6bdc4460a25bfba900190dbf44721e620c
Kim Vladimir <kimvova85@mail.ru>
2023-10-17 23:49:34 +1000

    Add current_user to helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  helper_method :signed_in?
+  helper_method :signed_in?, :current_user
 
   include AuthConcern
   include Pundit::Authorization
